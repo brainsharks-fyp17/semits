ca37e0430b19c8c46ff5b303de03ce7f096515ec
rumesh <rumesh.m.s16@gmail.com>
2021-09-21 10:31:43 +0530

    ppdb creating script updated

--- resource/create_ppdb_rules.py
+++ resource/create_ppdb_rules.py
@@ -1,13 +1,22 @@
-ppdb_lines = open("ppdb-si.txt").readlines()
-rules = []
-# comp-> simp rules
-comp_file = open("./denoise/comp_rules.txt", "w")
-for line in ppdb_lines:
-    if len(line) > 3 and "-" not in line and "," in line:
-        rules.append(line)
-        comp_file.write(line)
-comp_file.close()
-# simp->comp rules
+# ppdb_lines = open("ppdb-si.txt").readlines()
+# rules = []
+# # comp-> simp rules
+# comp_file = open("./denoise/comp_rules.txt", "w")
+# for line in ppdb_lines:
+#     if len(line) > 3 and "-" not in line and "," in line:
+#         rules.append(line)
+#         comp_file.write(line)
+# comp_file.close()
+# # simp->comp rules
+# simp_file = open("./denoise/simp_rules.txt", "w")
+# for rule in rules:
+#     rule = rule.split(",")
+#     comp = rule[0].strip()
+#     simp = rule[1].strip()
+#     simp_file.write(simp + "," + comp + "\n")
+# simp_file.close()
+
+rules = open("./denoise/comp_rules.txt").readlines()
 simp_file = open("./denoise/simp_rules.txt", "w")
 for rule in rules:
     rule = rule.split(",")
