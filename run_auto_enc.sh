7c2f02409a32f8f30c2574d46d13b2009cf54283
rumesh <rumesh.m.s16@gmail.com>
2021-09-21 10:55:38 +0530

    auro encoing script added

--- run_auto_enc.sh
+++ run_auto_enc.sh
@@ -109,7 +109,7 @@ python -u main.py \
 --use_multi_process 1 \
 --otf_num_processes 2 \
 --otf_sync_params_every 300 \
---us_pretrain_embedding 0 \
+--us_pretrain_embedding 1 \
 --embedding_path ${EMBEDDING_PATH} \
 --stopping_criterion 'sari' \
 --name ${NAME} \
@@ -123,7 +123,7 @@ python -u main.py \
 --beam_size ${BEAM_SIZE} \
 --supervised_rate ${SUPERVISED_RATE} \
 --rl_finetune  ${RL_FINETUNE} \
---use_pretrained_model 1 \
+--use_pretrained_model 0 \
 --gamma ${GAMMA} \
 --delta ${DELATA} \
 --otf_back_translation 1 \
