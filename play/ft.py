9e7b1a1bf1b1efc53b28c64a745e3fc4e6fb1e5e
rumesh <rumesh.m.s16@gmail.com>
2021-10-27 06:16:02 +0530

    new evaluation script

--- play/ft.py
+++ play/ft.py
@@ -2,9 +2,13 @@ from easse.sari import corpus_sari, get_corpus_sari_operation_scores
 from easse.bleu import corpus_bleu
 from easse.cli import report
 
-original = open("ori.txt").readlines()
-syst = open("pre.txt").readlines()
-ref = open("ref.txt").readlines()
+base = "/home/rumesh/Desktop/semits2/5/new2/"
+original =base+"comp.txt"
+sys_out = base + "si-cc_SUPERVISED_RATE_1_RL_FINETUNE_0"
+refr = base+"ref.txt"
+original = open(original).readlines()
+syst = open(sys_out).readlines()
+ref = open(refr).readlines()
 
 print("SARI Score using ref: ", corpus_sari(orig_sents=original,
                                             sys_sents=syst,
@@ -15,10 +19,10 @@ print("SARI components: add, keep ,del ", get_corpus_sari_operation_scores(orig_
                                                                            refs_sents=[ref]))
 print("BLEU: ", corpus_bleu(sys_sents=syst, refs_sents=[ref]))
 
-report(
-    "custom",
-    sys_sents_path="pre.txt",
-    orig_sents_path="ori.txt",
-    refs_sents_paths="ref.txt",
-    lowercase=False
-)
+# report(
+#     "custom",
+#     sys_sents_path=sys_out,
+#     orig_sents_path=original,
+#     refs_sents_paths=refr,
+#     lowercase=False
+# )
